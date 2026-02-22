f345bcb158e97abd47d3379d3a4eb64a9ed9077f
nkweeks <nkweeks@hawaii.edu>
2026-02-21 18:33:40 -1000

    feat(auth): configure custom cognito domain auth.benstagram.net with ACM certificate ARN

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,12 +2,25 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   storage,
+});
+
+// Configure Custom Domain for Cognito Hosted UI
+backend.auth.resources.userPool.addDomain('CustomDomain', {
+  customDomain: {
+    domainName: 'auth.benstagram.net',
+    certificate: Certificate.fromCertificateArn(
+      backend.auth.resources.userPool,
+      'BenstagramAuthCert',
+      'arn:aws:acm:us-east-1:951282861149:certificate/3e8c05a8-5274-4f8c-9e17-eed9f409d596'
+    )
+  }
 });
